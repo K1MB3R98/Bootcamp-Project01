89a20f266c5895f84407664699b97926fae3fe2f
KimberleySchubert <kimberley.schubert@gmail.com>
2022-03-07 05:29:07 -0500

    merging assg02 branch --> Comments section functionality

--- script.js
+++ script.js
@@ -67,60 +67,60 @@ document.querySelector('button').addEventListener('click', function(event) {
 
 
 
-    function currentDate() {
-        const date = new Date();
+function currentDate() {
+    const date = new Date();
 
-        // convert returned number to day of week
-        dayOfWeek = date.getDay();
-        const dayNames = [
-            "Sunday",
-            "Monday",
-            "Tuesday",
-            "Wednesday",
-            "Thursday",
-            "Friday",
-            "Saturday"
-        ];
-        const weekDay = dayNames[dayOfWeek];
+    // convert returned number to day of week
+    dayOfWeek = date.getDay();
+    const dayNames = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+    const weekDay = dayNames[dayOfWeek];
 
-        // convert returned number to month name
-        numberOfMonth = date.getMonth();
-        const monthNames = [
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-            "August",
-            "September",
-            "October",
-            "November",
-            "December"
-        ];
-        const month = monthNames[numberOfMonth];
+    // convert returned number to month name
+    numberOfMonth = date.getMonth();
+    const monthNames = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+    const month = monthNames[numberOfMonth];
 
-        // get the date #
-        const day = date.getDate();
+    // get the date #
+    const day = date.getDate();
 
-        // figure out suffix to apply to date #
-        numberSuffix = date.getDate();
-        if (numberSuffix == 1 || numberSuffix == 21 || numberSuffix == 31) {
-            suffix = "st";
-        } else if (numberSuffix == 2 || numberSuffix == 22) {
-            suffix = "nd";
-        } else if (numberSuffix == 3 || numberSuffix == 23) {
-            suffix = "rd";
-        } else {
-            suffix = "th";
-        };
-    
-        // get the full year 
-        const year = date.getFullYear();
+    // figure out suffix to apply to date #
+    numberSuffix = date.getDate();
+    if (numberSuffix == 1 || numberSuffix == 21 || numberSuffix == 31) {
+        suffix = "st";
+    } else if (numberSuffix == 2 || numberSuffix == 22) {
+        suffix = "nd";
+    } else if (numberSuffix == 3 || numberSuffix == 23) {
+        suffix = "rd";
+    } else {
+        suffix = "th";
+    };
 
-        // assemble the full date
-        const today = `${weekDay} ${month} ${day}${suffix}, ${year}`;
+    // get the full year 
+    const year = date.getFullYear();
 
-        return today;
-    }
+    // assemble the full date
+    const today = `${weekDay} ${month} ${day}${suffix}, ${year}`;
+
+    return today;
+} 
